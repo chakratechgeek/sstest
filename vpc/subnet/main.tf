5942e826a1fcfb4a0bc26b9c3777c9ec306b9def
chakra <chakratechgeek@outlook.com>
2025-07-31 12:18:46 +1000

    changed

---
+++ vpc/subnet/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+resource "aws_s3_bucket" "backend_bucket" {
+  bucket = "resource1-sub-300720299909"  # MUST be globally unique!
+  acl    = "private"
+  tags = {
+    Name        = "SeparateStateBucket"
+    Environment = "uat"
+  }
+}
