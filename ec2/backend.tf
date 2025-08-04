42ec098e0c8e4cde6901e99e60620f31e2297676
chakra <chakratechgeek@outlook.com>
2025-08-04 17:24:18 +1000

    oh man

--- ec2/backend.tf
+++ ec2/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket         = "statefile-chaks"  # <--- Use the new bucket name!
     key            = "terraform-state/10-uat/terraform.tfstate"  # Or another unique path for this deployment
-    region         = "ap-southeast-2" #sydney
+    region         = "ap-southeast-2" #sydney only
   }
 }
